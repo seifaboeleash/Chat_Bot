5dee244b1c26997fd6ee184db6a8db8016d9f830
Seif Abo Eleash <seifaboeleish@gmail.com>
2026-07-16 19:22:06 +0300

    feat: add AppConstants to manage Gemini API key and base URL configuration

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
   static String get geminiApiKey => dotenv.env['GEMINI_API_KEY'] ?? '';
-  
-  static const String baseUrl =
-      'https://generativelanguage.googleapis.com/v1beta/';
+  static const String baseUrl = 'https://generativelanguage.googleapis.com/v1beta/';
 }
